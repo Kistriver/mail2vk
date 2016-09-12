034d476811f7ed5f30106f55cf5a3d7c76056113
Alexey Kachalov <kachalov@kistriver.com>
2016-09-13 00:03:07 +0300

    Main module test skeleton

---
+++ tests/main.py
@@ -0,0 +1,13 @@
+import unittest2
+from mock import patch, Mock
+import imaplib
+
+from mail2vk import __main__
+
+
+class MainCase(unittest2.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest2.main()
